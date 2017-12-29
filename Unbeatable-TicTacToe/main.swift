cac27f028505d7f1e795cdcf1535b91e5dafa981
Danny McAteer <mcateerd2@gmail.com>
2017-12-29 17:10:09 -0500

    Initial Commit

---
+++ Unbeatable-TicTacToe/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Unbeatable-TicTacToe
+//
+//  Created by Daniel McAteer on 12/29/17.
+//  Copyright © 2017 Daniel McAteer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
